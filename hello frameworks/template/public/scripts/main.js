6ee4ff06cb2cef4f8c54b96ae95966ca3283712d
raglanma <raglanma@rose-hulman.edu>
2021-09-13 16:21:29 -0400

    true and false

--- hello frameworks/template/public/scripts/main.js
+++ hello frameworks/template/public/scripts/main.js
@@ -9,28 +9,31 @@
 /** namespace. */
 var rhit = rhit || {};
 
-/** globals */
-rhit.variableName = "";
+rhit.counter = 0;
 
 /** function and class syntax examples */
-rhit.functionName = function () {
-	/** function body */
-};
+// rhit.functionName = function () {
+// 	/** function body */
+// 	console.log("Ready")
+// };
 
-rhit.ClassName = class {
-	constructor() {
-
-	}
-
-	methodName() {
-
-	}
-}
-
-/* Main */
-/** function and class syntax examples */
 rhit.main = function () {
-	console.log("Ready");
+	//console.log("Ready");
+	let buttons = document.querySelectorAll("#counterButtons button");
+	// for (let i = 0; i < buttons.length; i++) {
+	// 	const button = buttons[i]
+	// 	button.onclick = (event) => {
+	// 		console.log(`You pressed`, button)
+	// 	}
+	// }
+
+	for (const button of buttons) {
+		button.onclick = (event) => {
+			const dataAmount = parseInt(button.dataset.amount);
+			const dataIsMultiplication = button.dataset.isMultiplication === "true";
+			console.log(`Amount: ${dataAmount}, Mult: ${dataIsMultiplication}`);
+		}
+	}
 };
 
 rhit.main();
