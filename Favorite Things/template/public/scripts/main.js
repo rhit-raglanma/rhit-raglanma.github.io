021db548bd06c52a5388eae83580ad45a591958b
raglanma <raglanma@rose-hulman.edu>
2021-09-20 16:48:54 -0400

    favorite things finished

--- Favorite Things/template/public/scripts/main.js
+++ Favorite Things/template/public/scripts/main.js
@@ -11,26 +11,65 @@ var rhit = rhit || {};
 
 /** globals */
 rhit.variableName = "";
+rhit.counter = 0;
 
 /** function and class syntax examples */
 rhit.functionName = function () {
 	/** function body */
 };
 
-rhit.ClassName = class {
-	constructor() {
+// rhit.ClassName = class {
+// 	constructor() {
 
-	}
+// 	}
 
-	methodName() {
+// 	methodName() {
 
-	}
-}
+// 	}
+// }
 
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+
+	const minus = document.getElementById("minus");
+	const reset = document.getElementById("reset");
+	const plus = document.getElementById("plus");
+	let counter = document.getElementById("count");
+
+	minus.onclick = (event) => {
+		rhit.counter -= 1;
+		counter.innerHTML = `${rhit.counter}`;
+	}
+	reset.onclick = (event) => {
+		rhit.counter = 0;
+		counter.innerHTML = `${rhit.counter}`;
+	}
+	plus.onclick = (event) => {
+		rhit.counter += 1;
+		counter.innerHTML = `${rhit.counter}`;
+	}
+
+
+	const blue = document.getElementById("blue");
+	const red = document.getElementById("red");
+	const green = document.getElementById("green");
+	const purple = document.getElementById("purple");
+	let color = document.getElementById("outer-color-div")
+
+	blue.onclick = (event) => {
+		color.innerHTML = `<div id="color-div" style="background-color:blue">BLUE</div>`;
+	}
+	red.onclick = (event) => {
+		color.innerHTML = `<div id="color-div" style="background-color:red">RED</div>`;
+	}
+	green.onclick = (event) => {
+		color.innerHTML = `<div id="color-div" style="background-color:green">GREEN</div>`;
+	}
+	purple.onclick = (event) => {
+		color.innerHTML = `<div id="color-div" style="background-color:purple">PURPLE</div>`;
+	}
 };
 
 rhit.main();
