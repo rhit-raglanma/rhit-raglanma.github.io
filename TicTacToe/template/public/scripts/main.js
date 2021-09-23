43e1708134f8bc314af4365663f0a902ee8ea72a
raglanma <raglanma@rose-hulman.edu>
2021-09-23 15:45:26 -0400

    enums

--- TicTacToe/template/public/scripts/main.js
+++ TicTacToe/template/public/scripts/main.js
@@ -19,7 +19,7 @@ rhit.functionName = function () {
 
 rhit.pageController = class {
 	constructor() {
-		
+		this.game = new rhit.Game();
 	}
 
 	UpdateView() {
@@ -28,8 +28,29 @@ rhit.pageController = class {
 }
 
 rhit.Game = class {
+
+	static Mark = {
+		X: "X",
+		O: "O",
+		NONE: " ",
+	}
+
+	static State = {
+		X_TURN: "X's Turn",
+		O_TURN: "O's Turn",
+		X_WIN: "X Wins!",
+		O_WIN: "O Wins!",
+		TIE: "Tie Game",
+	}
+
 	constructor() {
-		
+		this.state = rhit.Game.State.X_TURN;
+		this.board = [];
+		for (let k = 0; k < 9; k++) {
+			this.board.push(rhit.Game.Mark.NONE);
+		}
+		console.log('this.board = ', this.board);
+		console.log('this.state = ', this.state);
 	}
 
 	pressedButtonAtIndex(buttonIndex) {
