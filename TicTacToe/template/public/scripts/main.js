19dcaf3350c7f59b66c000a1b03566a508c4b202
raglanma <raglanma@rose-hulman.edu>
2021-09-23 16:42:41 -0400

    final commit

--- TicTacToe/template/public/scripts/main.js
+++ TicTacToe/template/public/scripts/main.js
@@ -25,14 +25,26 @@ rhit.pageController = class {
 
 		for (const square of squares) {
 			square.onclick = (event) => {
-				const buttonIndex = square.dataset.buttonIndex;
+				const buttonIndex = parseInt(square.dataset.buttonIndex);
 				this.game.pressedButtonAtIndex(buttonIndex);
+				//console.log(buttonIndex);
+				this.UpdateView();
 			}
 		}
+
+		document.querySelector("#newGameButton").onclick = (event) => {
+			this.game = new rhit.Game();
+			this.UpdateView();
+		}
+		this.UpdateView();
 	}
 
 	UpdateView() {
-
+		const squares = document.querySelectorAll(".square");
+		squares.forEach((square, index) => {
+			square.innerHTML = this.game.board[index];
+		});
+		document.querySelector("#gameStateText").innerHTML = this.game.state;
 	}
 }
 
@@ -58,13 +70,58 @@ rhit.Game = class {
 		for (let k = 0; k < 9; k++) {
 			this.board.push(rhit.Game.Mark.NONE);
 		}
-		console.log('this.board = ', this.board);
-		console.log('this.state = ', this.state);
+		// console.log('this.board = ', this.board);
+		// console.log('this.state = ', this.state);
 	}
 
 	pressedButtonAtIndex(buttonIndex) {
-		this.board[buttonIndex] = rhit.Game.Mark.X;
-		
+		//this.board[buttonIndex] = rhit.Game.Mark.X;
+		if (this.state == rhit.Game.State.X_WIN || 
+			this.state == rhit.Game.State.O_WIN || 
+			this.state == rhit.Game.State.TIE) {
+			return;
+		}
+		if (this.board[buttonIndex] != rhit.Game.Mark.NONE) {
+			return;
+		}
+		if (this.state == rhit.Game.State.X_TURN) {
+			this.board[buttonIndex] = rhit.Game.Mark.X;
+			this.state = rhit.Game.State.O_TURN;
+		} else {
+			this.board[buttonIndex] = rhit.Game.Mark.O;
+			this.state = rhit.Game.State.X_TURN;
+			
+		}
+		this._checkForGameOver();
+
+	}
+
+	_checkForGameOver() {
+
+		if (!this.board.includes(rhit.Game.Mark.NONE)) {
+			this.state = rhit.Game.State.TIE;
+		}
+
+
+		const linesOf3 = [];
+		linesOf3.push(this.board[0] + this.board[1] + this.board[2]);
+		linesOf3.push(this.board[3] + this.board[4] + this.board[5]);
+		linesOf3.push(this.board[6] + this.board[7] + this.board[8]);
+
+		linesOf3.push(this.board[0] + this.board[3] + this.board[6]);
+		linesOf3.push(this.board[1] + this.board[4] + this.board[7]);
+		linesOf3.push(this.board[2] + this.board[5] + this.board[8]);
+
+		linesOf3.push(this.board[0] + this.board[4] + this.board[8]);
+		linesOf3.push(this.board[2] + this.board[1] + this.board[6]);
+
+		for (const lineOf3 of linesOf3) {
+			if (lineOf3 == "XXX") {
+				this.state = rhit.Game.State.X_WIN;
+			} else if (lineOf3 == "OOO") {
+				this.state = rhit.Game.State.O_WIN;
+			}
+		}
 	}
 
 	getMarkAtIndex(buttonIndex) {
