e0cfeda6764f5c20d40fb4b509464b24e0787d32
raglanma <raglanma@rose-hulman.edu>
2021-10-20 21:14:39 -0400

    pic page working

--- photoBucket/template/public/scripts/main.js
+++ photoBucket/template/public/scripts/main.js
@@ -16,6 +16,7 @@ rhit.FB_KEY_CAPTION = "caption";
 rhit.FB_KEY_URL = "imageUrl";
 rhit.FB_KEY_LAST_TOUCHED = "lastTouched";
 rhit.fbPicsManager = null;
+rhit.fbSinglePicManager = null;
 
 // functions
 function htmlToElement(html) {
@@ -153,6 +154,127 @@ rhit.FbPicsManager = class {
 }
 
 
+
+rhit.DetailPageController = class {
+	constructor() {
+		console.log("detail");
+
+
+
+		document.querySelector("#submitEditURL").onclick = (event) => {
+
+			const url = document.querySelector("#inputUrl").value;
+			const caption = document.querySelector("#inputCaption").value;
+			// console.log(quote);
+			// console.log(movie);
+			rhit.fbSinglePicManager.update(url, caption);
+		}
+
+		$("#editUrlDialog").on("show.bs.modal", (event) => {
+			document.querySelector("#inputUrl").value = rhit.fbSinglePicManager.pic;
+			document.querySelector("#inputCaption").value = rhit.fbSinglePicManager.caption;
+		})
+		$("#editUrlDialog").on("shown.bs.modal", (event) => {
+			document.querySelector("#inputUrl").focus();
+		})
+
+
+
+		document.querySelector("#submitDeleteUrl").onclick = (event) => {
+
+
+			rhit.fbSinglePicManager.delete().then(function () {
+				console.log("deleted");
+				window.location.href = "/";
+			}).catch(function (error) {
+				console.log("error deleting document");
+			});
+		}
+
+
+		rhit.fbSinglePicManager.beginListening(this.updateView.bind(this));
+
+
+
+
+	}
+	updateView() {
+		console.log("update view");
+		// document.querySelector(".detail-image").innerHTML = rhit.fbSinglePicManager.pic;
+		// document.querySelector(".caption").innerHTML = rhit.fbSinglePicManager.caption;
+		document.querySelector(".pin").innerHTML = `<img src="${rhit.fbSinglePicManager.pic}" alt="${rhit.fbSinglePicManager.caption}">
+		<p class="caption">${rhit.fbSinglePicManager.caption}</p>`;
+	}
+}
+
+
+rhit.FbSinglePicManager = class {
+	constructor(PicId) {
+		this._documentSnapshot = {};
+		this._unsubscribe = null;
+		this._ref = firebase.firestore().collection(rhit.FB_COLLECTION_PICS).doc(PicId);
+		console.log("listening");
+	}
+	beginListening(changeListener) {
+
+		this._unsubscribe = this._ref.onSnapshot((doc) => {
+			if (doc.exists) {
+				console.log("Document data:", doc.data());
+				this._documentSnapshot = doc;
+				changeListener();
+			} else {
+				console.log("no such doc");
+			}
+		})
+
+
+
+
+
+
+	}
+	stopListening() {
+		this._unsubscribe();
+	}
+	update(url, caption) {
+
+		this._ref.update({
+			[rhit.FB_KEY_URL]: url,
+			[rhit.FB_KEY_CAPTION]: caption,
+			[rhit.FB_KEY_LAST_TOUCHED]: firebase.firestore.Timestamp.now(),
+		})
+			.then(function (docRef) {
+				//console.log("Document written with ID: ", docRef.id);
+			})
+			.catch(function (error) {
+				//console.error("Error adding document: ", error);
+			})
+
+	}
+
+	get pic() {
+		return this._documentSnapshot.get(rhit.FB_KEY_URL);
+	}
+
+	get caption() {
+		return this._documentSnapshot.get(rhit.FB_KEY_CAPTION);
+	}
+
+	delete() {
+
+		return this._ref.delete();
+		// .then(function() {
+
+		// }).catch(function (error) {
+		// 	console.log("error deleting document");
+		// });
+
+	}
+}
+
+
+
+
 /* Main */
 /** function and class syntax examples */
 rhit.main = function () {
@@ -165,6 +287,28 @@ rhit.main = function () {
 
 	}
 
+	if (document.querySelector("#detailPage")) {
+		console.log("detail page");
+		//const movieQuoteId = rhit.storage.getMovieQuoteId();
+
+		const queryString = window.location.search;
+
+		const urlParams = new URLSearchParams(queryString);
+		const PicId = urlParams.get("id");
+
+
+		console.log(`Detail page for ${PicId}`);
+
+		if (!PicId) {
+			console.log("error");
+			window.location.href = "/";
+		}
+
+		rhit.fbSinglePicManager = new rhit.FbSinglePicManager(PicId);
+		new rhit.DetailPageController();
+
+
+	}
 
 
 
