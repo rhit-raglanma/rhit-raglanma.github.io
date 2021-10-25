eea3037a1a139ec87b949d474a802ebcacc27c3f
raglanma <raglanma@rose-hulman.edu>
2021-10-25 15:50:57 -0400

    final node commit

--- Node/NodeIntro/npmAsciiFun.js
+++ Node/NodeIntro/npmAsciiFun.js
@@ -1,12 +1,23 @@
 console.log("todo");
 
-var figlet = require('figlet');
+// var figlet = require('figlet');
 
-figlet('Hello World!!', function(err, data) {
-    if (err) {
-        console.log('Something went wrong...');
-        console.dir(err);
-        return;
-    }
-    console.log(data)
-});
+// figlet('CSSE280 using node.js', function(err, data) {
+//     if (err) {
+//         console.log('Something went wrong...');
+//         console.dir(err);
+//         return;
+//     }
+//     console.log(data)
+// });
+
+const imgToAscii = require('ascii-img-canvas-nodejs')
+
+const opts = {}
+
+
+
+const asciiImgLocal = imgToAscii('files/node.svg.png', opts);
+asciiImgLocal.then( (asciiImgLocal) => {
+    console.log(asciiImgLocal);
+})
