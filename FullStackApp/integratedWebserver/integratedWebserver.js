b436103b76347f33a7c3c0a3944f900dd8b07c16
raglanma <raglanma@rose-hulman.edu>
2021-11-02 17:13:36 -0400

    final eS

--- FullStackApp/integratedWebserver/integratedWebserver.js
+++ FullStackApp/integratedWebserver/integratedWebserver.js
@@ -15,9 +15,6 @@ fs.readFile(serverSideStorage, function(err, buf) {
         console.log("error: ", err);
     } else {
         data = JSON.parse(buf.toString());
-        if (data.length != 0) {
-            counter = data[data.length-1];
-        }
     }
     console.log("data read from file");
 });
@@ -36,7 +33,6 @@ app.use('/static', express.static("public"));
  
 
 
-
 //middleware
 var bodyParser = require("body-parser");
 
@@ -48,18 +44,59 @@ app.use('/api/', bodyParser.json() );
 // appendFile
 
 
-app.post('/pug/hello', function(req, res) {
-    counter = req.body.count || counter;
-    data.push(counter);
+//READ ALL
+app.get("/api/", function(req, res) {
+    res.send(data);
+    res.end();
+});
+
+
+//CREATE
+app.post("/api/", function(req, res) {
+    let name = req.body.name;
+    let counter = req.body.count;
+    data.push({"name": name, "count": counter});
     saveToServer(data);
-
-    res.render('hello', {title: "Hello Button", count: counter});
-
+    res.send("post successful");
+    res.end();
 });
 
-app.get('/pug/history', function(req, res) {
-    res.render('history', {title: 'Count history', data: data});
+//READ ONE
+app.get("/api/id/:id", function(req, res) {
+    let id = parseInt(req.params.id);
+    let result = data[id];
+    res.send(result);
+    res.end();
+}).put("/api/id/:id", function(req, res) {
+    let id = parseInt(req.params.id);
+
+    let name = req.body.name;
+    let counter = req.body.count;
+    data[id] = {"name": name, "count": counter};
+    saveToServer(data);
+    res.send("put successful");
+    res.end();
+}).delete("/api/id/:id", function(req, res) {
+    let id = parseInt(req.params.id);
+    data.splice(id,1);
+    saveToServer(data);
+    res.send("delete successful");
+    res.end();
+})
+
+
+// app.post('/pug/hello', function(req, res) {
+//     counter = req.body.count || counter;
+//     data.push(counter);
+//     saveToServer(data);
+
+//     res.render('hello', {title: "Hello Button", count: counter});
+
+// });
+
+// app.get('/pug/history', function(req, res) {
+//     res.render('history', {title: 'Count history', data: data});
     
-});
+// });
 
 app.listen(3000);
